ad71ef6bc5ff98e76307893d14c493614aefcf81
Yulia Iakovleva <yulia_iakovleva@proton.me>
2023-05-25 16:55:08 +0200

    minor changes

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image, ImageDraw
+from PIL import Image
 
 
 # Extending User Model Using a One-To-One Link
